a9bd4149900069bc45e8498736a119b814abf11c
Sourav Das <souravdas007123@gmail.com>
2025-03-22 22:27:57 +0530

    inner page create link

--- system/urls.py
+++ system/urls.py
@@ -3,13 +3,23 @@ from system import views
 urlpatterns = [
     path('', views.home,name='home'),
     path('unit/', views.unit,name='unit'),
+    path('unitdata/', views.unit_data,name='unitdata'),
+    path('unitadd/', views.unit_add,name='unitadd'),
     path('product/', views.product,name='product'),
+    path('productdata/', views.product_data,name='productdata'),
+    path('productadd/', views.product_add,name='productadd'),
     path('vendor/', views.vendor,name='vendor'),
+    path('vendordata/', views.vendor_data,name='vendordata'),
+    path('vendoradd/', views.vendor_add,name='vendoradd'),
     path('customer/', views.customer,name='customer'),
     path('customerdata/', views.customer_data,name='customerdata'),
     path('customeradd/', views.customer_add,name='customeradd'),
     path('purchase/', views.purchase,name='purchase'),
+    path('purchasedata/', views.purchase_data,name='purchasedata'),
+    path('purchaseadd/', views.purchase_add,name='purchaseadd'),
     path('sale/', views.sale,name='sale'),
+    path('saledata/', views.sale_data,name='saledata'),
+    path('saleadd/', views.sale_add,name='saleadd'),
     path('setting/', views.setting,name='setting'),
 
 
--- system/views.py
+++ system/views.py
@@ -5,16 +5,34 @@ def home(request):
     return render(request,'system/base_structure.html')
 
 def unit(request):
-    return render(request,'system/unit.html')
+    return render(request,'system/unit_data.html')
+
+def unit_data(request):
+    return render(request,'system/unit_data.html')
+
+def unit_add(request):
+    return render(request,'system/unit_add.html')
 
 def product(request):
-    return render(request,'system/product.html')
+    return render(request,'system/product_data.html')
+
+def product_data(request):
+    return render(request,'system/product_data.html')
+
+def product_add(request):
+    return render(request,'system/product_add.html')
 
 def vendor(request):
-    return render(request,'system/vendor.html')
+    return render(request,'system/vendor_data.html')
+
+def vendor_data(request):
+    return render(request,'system/vendor_data.html')
+
+def vendor_add(request):
+    return render(request,'system/vendor_add.html')
 
 def customer(request):
-    return render(request,'system/customer.html')
+    return render(request,'system/customer_data.html')
 
 def customer_data(request):
     return render(request,'system/customer_data.html')
@@ -23,10 +41,22 @@ def customer_add(request):
     return render(request,'system/customer_add.html')
 
 def purchase(request):
-    return render(request,'system/purchase.html')
+    return render(request,'system/purchase_data.html')
+
+def purchase_data(request):
+    return render(request,'system/purchase_data.html')
+
+def purchase_add(request):
+    return render(request,'system/purchase_add.html')
 
 def sale(request):
-    return render(request,'system/sale.html')
+    return render(request,'system/sale_data.html')
+
+def sale_data(request):
+    return render(request,'system/sale_data.html')
+
+def sale_add(request):
+    return render(request,'system/sale_add.html')
 
 def setting(request):
     return render(request,'system/setting.html')
